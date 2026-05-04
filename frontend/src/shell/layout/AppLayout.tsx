b97b64be65bcfe4f60d161810c37a40d8eaa28a7
JoZeuZz <joserodriguez.civilinformatico@gmail.com>
2026-05-04 18:24:15 -0400

    feat: eliminar link Devoluciones del sidebar del supervisor

--- frontend/src/shell/layout/AppLayout.tsx
+++ frontend/src/shell/layout/AppLayout.tsx
@@ -262,16 +262,6 @@ const AppLayout = () => {
         </svg>
         <span className={!isSidebarOpen ? 'lg:hidden' : ''}>Operación Diaria</span>
       </NavLink>
-      <NavLink
-        to="/supervisor/devoluciones"
-        onClick={handleLinkClick}
-        className={({ isActive }) => (isActive ? activeLinkClass : linkClass)}
-      >
-        <svg className={`w-5 h-5 ${isSidebarOpen ? 'mr-3' : ''}`} fill="none" stroke="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
-        </svg>
-        <span className={!isSidebarOpen ? 'lg:hidden' : ''}>Devoluciones</span>
-      </NavLink>
     </Fragment>
   );
 
