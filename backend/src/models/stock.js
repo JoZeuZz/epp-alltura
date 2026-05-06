73fd797d7ba1c329cb516177d42087325616f5e6
JoZeuZz <joserodriguez.civilinformatico@gmail.com>
2026-05-06 09:22:41 -0400

    feat: update StockModel to use bodega_id

--- backend/src/models/stock.js
+++ backend/src/models/stock.js
@@ -3,68 +3,46 @@ const db = require('../db');
 class StockModel {
   constructor(data) {
     this.id = data.id;
-    this.ubicacion_id = data.ubicacion_id;
+    this.bodega_id = data.bodega_id;
     this.articulo_id = data.articulo_id;
     this.cantidad_disponible = data.cantidad_disponible;
     this.cantidad_reservada = data.cantidad_reservada;
     this.actualizado_en = data.actualizado_en;
   }
 
-  static async getByClave({ ubicacion_id, articulo_id }) {
+  static async getByClave({ bodega_id, articulo_id }) {
     const { rows } = await db.query(
-      `
-      SELECT *
-      FROM stock
-      WHERE ubicacion_id = $1
-        AND articulo_id = $2
-        AND lote_id IS NULL
-      `,
-      [ubicacion_id, articulo_id]
+      `SELECT * FROM stock
+       WHERE bodega_id = $1 AND articulo_id = $2 AND lote_id IS NULL`,
+      [bodega_id, articulo_id]
     );
-
     return rows.length ? new StockModel(rows[0]) : null;
   }
 
-  static async upsertDelta({
-    ubicacion_id,
-    articulo_id,
-    disponible_delta = 0,
-    reservada_delta = 0,
-  }) {
+  static async upsertDelta({ bodega_id, articulo_id, disponible_delta = 0, reservada_delta = 0 }) {
     const query = `
-      INSERT INTO stock (
-        ubicacion_id, articulo_id, lote_id, cantidad_disponible, cantidad_reservada
-      )
+      INSERT INTO stock (bodega_id, articulo_id, lote_id, cantidad_disponible, cantidad_reservada)
       VALUES ($1, $2, NULL, GREATEST($3, 0), GREATEST($4, 0))
-      ON CONFLICT (ubicacion_id, articulo_id) WHERE lote_id IS NULL
+      ON CONFLICT (bodega_id, articulo_id) WHERE lote_id IS NULL
       DO UPDATE SET
         cantidad_disponible = GREATEST(stock.cantidad_disponible + $3, 0),
         cantidad_reservada = GREATEST(stock.cantidad_reservada + $4, 0),
         actualizado_en = NOW()
       RETURNING *
     `;
-
-    const params = [ubicacion_id, articulo_id, disponible_delta, reservada_delta];
-    const { rows } = await db.query(query, params);
+    const { rows } = await db.query(query, [bodega_id, articulo_id, disponible_delta, reservada_delta]);
     return new StockModel(rows[0]);
   }
 
-  static async listByUbicacion(ubicacionId) {
+  static async listByBodega(bodegaId) {
     const { rows } = await db.query(
-      `
-      SELECT
-        s.*,
-        a.nombre AS articulo_nombre,
-        a.grupo_principal AS articulo_tipo
-      FROM stock s
-      INNER JOIN articulo a ON a.id = s.articulo_id
-      WHERE s.ubicacion_id = $1
-        AND s.lote_id IS NULL
-      ORDER BY a.nombre ASC
-      `,
-      [ubicacionId]
+      `SELECT s.*, a.nombre AS articulo_nombre, a.grupo_principal AS articulo_tipo
+       FROM stock s
+       INNER JOIN articulo a ON a.id = s.articulo_id
+       WHERE s.bodega_id = $1 AND s.lote_id IS NULL
+       ORDER BY a.nombre ASC`,
+      [bodegaId]
     );
-
     return rows;
   }
 }
